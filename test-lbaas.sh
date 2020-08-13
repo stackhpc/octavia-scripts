581dae37a5665eff0f322c22a647ec5e2d9334be
Bharat Kunwar <bharat@stackhpc.com>
2020-08-13 13:55:18 +0000

    Separate server name variable

--- test-lbaas.sh
+++ test-lbaas.sh
@@ -3,6 +3,7 @@ set -eux
 
 LB_NAME=${LB_NAME:-demo-lb}
 LB_FIP=${LB_FIP:-10.60.253.77}
+SERVER_NAME=${SERVER_NAME:-demo-lb}
 SERVER_FIP=${SERVER_FIP:-10.60.253.41}
 SERVER_IMAGE=${SERVER_IMAGE:-CentOS7}
 SERVER_FLAVOR=${SERVER_FLAVOR:-general.v1.small}
@@ -10,13 +11,15 @@ SERVER_KEYNAME=${SERVER_KEYNAME:-wendy}
 SERVER_NET=${SERVER_NET:-p3-internal}
 SERVER_SUBNET=${SERVER_SUBNET:-p3-internal}
 
-openstack server show $LB_NAME || {
-    openstack server create $LB_NAME --image $SERVER_IMAGE --key-name $SERVER_KEYNAME --network $SERVER_NET --flavor $SERVER_FLAVOR --wait
+openstack server show $SERVER_NAME || {
+    openstack server create $SERVER_NAME --image $SERVER_IMAGE --key-name $SERVER_KEYNAME --network $SERVER_NET --flavor $SERVER_FLAVOR --wait
 }
-openstack server add floating ip $LB_NAME $SERVER_FIP
-ssh -o StrictHostKeyChecking=no centos@$SERVER_FIP sudo yum install -y nginx
-ssh -o StrictHostKeyChecking=no centos@$SERVER_FIP sudo systemctl start nginx
-ADDRESS=`openstack server show $LB_NAME -c addresses -f value | tr '; ' '\n' | grep $SERVER_NET | cut -f2 -d= | cut -f1 -d,`
+openstack server add floating ip $SERVER_NAME $SERVER_FIP
+until curl $SERVER_FIP; do
+    sleep 1
+    ssh -o StrictHostKeyChecking=no centos@$SERVER_FIP "sudo nohup /usr/libexec/platform-python -m http.server 80 &"
+done
+ADDRESS=`openstack server show $SERVER_NAME -c addresses -f value | tr '; ' '\n' | grep $SERVER_NET | cut -f2 -d= | cut -f1 -d,`
 
 openstack loadbalancer show $LB_NAME && openstack loadbalancer delete --cascade $LB_NAME
 openstack loadbalancer create --name $LB_NAME --vip-subnet-id $SERVER_SUBNET --wait
